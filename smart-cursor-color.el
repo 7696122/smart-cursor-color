b428f06668841e18da77c4b55435326047eab633
7696122 <7696122@gmail.com>
2014-06-30 23:19:02 +0900

    Change arg.

--- smart-cursor-color.el
+++ smart-cursor-color.el
@@ -25,12 +25,12 @@
 ;; to Emacs initialisation file (~/.emacs or ~/.emacs.d/init.el):
 ;;
 ;; If installed from melpa.
-;;       (smart-cursor-color-mode +1)
+;;       (smart-cursor-color-mode 1)
 ;;
 ;; If installed manually,
 ;;       (add-to-list 'load-path "path-to-installed-directory")
 ;;       (require 'smart-cursor-color)
-;;       (smart-cursor-color-mode +1)
+;;       (smart-cursor-color-mode 1)
 ;;
 ;; When hl-line-mode is on,
 ;; smart-cursor-color-mode is not work.
@@ -39,8 +39,8 @@
 ;; 
 ;; But when global-hl-line-mode is on,
 ;; smart-cursor-color-mode is work.
-;;       (global-hl-line-mode +1)
-;;       (smart-cursor-color-mode +1)
+;;       (global-hl-line-mode 1)
+;;       (smart-cursor-color-mode 1)
 ;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
